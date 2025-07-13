5c7682c9a612b0147eb93c52fd898632032456eb
Pawandasila <pawandasila06@gmail.com>
2025-07-13 20:08:33 +0530

    fixed inngest related issues and added imagekit

--- inngest/functions.ts
+++ inngest/functions.ts
@@ -1,4 +1,5 @@
 import { inngest } from "./client";
+import ImageKit from "imagekit";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
@@ -6,8 +7,14 @@ export const helloWorld = inngest.createFunction(
   async ({ event, step }) => {
     await step.sleep("wait-a-moment", "1s");
     return { message: `Hello ${event.data.email}!` };
-  },
+  }
 );
+
+const imageKit = new ImageKit({
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY || "",
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY || "",
+  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT || "",
+});
 
 export const generateAiThumbnail = inngest.createFunction(
   { id: "ai/generate-thumbnail" },
@@ -15,10 +22,40 @@ export const generateAiThumbnail = inngest.createFunction(
   async ({ event, step }) => {
     const { content, referenceImage, faceImage, userEmail } = event.data;
 
-    // Call your thumbnail generation logic here
-    // For example:
-    // const thumbnail = await generateThumbnail(content, referenceImage, faceImage, userEmail);
+    const uploadImageUrl = await step.run("Upload Image", async () => {
+      const results: { referenceImageUrl?: string; faceImageUrl?: string } = {};
 
-    return { message: "Thumbnail generated successfully", data: { content, referenceImage, faceImage, userEmail } };
-  },
+      if (referenceImage) {
+        const refImageUrl = await imageKit.upload({
+          file: referenceImage?.buffer ?? "",
+          fileName: `ref-${Date.now()}-${referenceImage.name || "image.jpg"}`,
+          folder: "/thumbnails/reference",
+          isPublished: true,
+          useUniqueFileName: false,
+        });
+
+        results.referenceImageUrl = refImageUrl.url;
+      }
+
+      if (faceImage?.buffer) {
+        const faceImageResponse = await imageKit.upload({
+          file: faceImage.buffer,
+          fileName: `face-${Date.now()}-${faceImage.name || "face.jpg"}`,
+          folder: "/thumbnails/faces",
+          isPublished: true,
+          useUniqueFileName: true,
+        });
+        results.faceImageUrl = faceImageResponse.url;
+      }
+
+      return {
+        results
+      };
+    });
+
+    return {
+      message: "Thumbnail generated successfully",
+      data: { content, referenceImage, faceImage, userEmail, uploadImageUrl },
+    };
+  }
 );
