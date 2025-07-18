41f49a5450ac907fd339fc4290ada0602ff76e1c
Pawandasila <pawandasila06@gmail.com>
2025-07-18 15:45:12 +0530

    fixed ingges issue

--- inngest/client.ts
+++ inngest/client.ts
@@ -1,4 +1,7 @@
 import { Inngest } from "inngest";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "TrendTide" });
+export const inngest = new Inngest({ 
+  id: "TrendTide",
+  eventKey: process.env.INNGEST_EVENT_KEY,
+});
